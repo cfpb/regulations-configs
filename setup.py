e88b03898a1aef8bdb4a99e18e40cb20c8f77ba2
Will Barton <william.barton@cfpb.gov>
2015-07-29 16:20:42 -0400

    Make it a Python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='regulations-configs',
+    version='0.1.0',
+    description='CFPB-specific configuration for the eRegulations parser',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    packages=find_packages(),
+)
+
