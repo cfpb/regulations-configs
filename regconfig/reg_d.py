2947a2c9b6348d248e3ae740722d6a7aa04327c0
Adam Scott <adam.scott@cfpb.gov>
2015-07-29 14:50:31 -0400

    add reg d included definitions

---
+++ regconfig/reg_d.py
@@ -0,0 +1,12 @@
+from regparser.default_settings import *
+
+#### Regulation D
+
+INCLUDE_DEFINITIONS_IN_PART_1004 = [
+    ('Alternative mortgage transaction', 'Alternative mortgage transaction'),
+    ('Creditor', 'Creditor'),
+    ('State', 'State'),
+    ('State law', 'State law'),
+]
+
+INCLUDE_DEFINITIONS_IN['1004'] = INCLUDE_DEFINITIONS_IN_PART_1004
