452cd343c59e332c864226b152490d03fb5510b4
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2015-07-30 16:00:32 -0400

    paragraph hierarchy for D

--- regconfig/reg_d.py
+++ regconfig/reg_d.py
@@ -7,3 +7,15 @@ INCLUDE_DEFINITIONS_IN_PART_1004 = [
     ('State law', 'State law'),
 ]
 
+PARAGRAPH_HIERARCHY_1004 = {
+    '1004.2': [
+        1,
+        1,
+        2, 2, 2,
+        1,
+        1,
+        2, 2, 2, 2,
+        1,
+        1,
+    ],
+}
