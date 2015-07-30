57d09c02f12e7b2f370b33fc9f8f2829cad32718
Will Barton <william.barton@cfpb.gov>
2015-07-30 16:19:23 -0400

    Remove added spaces.

--- regconfig/reg_d.py
+++ regconfig/reg_d.py
@@ -1,18 +1,18 @@
 #### Regulation D
 
 INCLUDE_DEFINITIONS_IN_PART_1004 = [
-     ('Creditor', 'Creditor shall have the same meaning as in 12 CFR 226.2'),
+    ('Creditor', 'Creditor shall have the same meaning as in 12 CFR 226.2'),
 ]
 
 PARAGRAPH_HIERARCHY_1004 = {
-     '1004.2': [
-         1,
-         1,
-         2, 2, 2,
-         1,
-         1,
-         2, 2, 2, 2,
-         1,
-         1,
-     ],
+    '1004.2': [
+        1,
+        1,
+        2, 2, 2,
+        1,
+        1,
+        2, 2, 2, 2,
+        1,
+        1,
+    ],
 }
