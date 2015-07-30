ba3210b802c9cc1395edee1ad84cea025cc275cf
Will Barton <william.barton@cfpb.gov>
2015-07-30 16:18:10 -0400

    Fix reg d included definitions

--- regconfig/reg_d.py
+++ regconfig/reg_d.py
@@ -1,21 +1,18 @@
 #### Regulation D
 
 INCLUDE_DEFINITIONS_IN_PART_1004 = [
-    ('Alternative mortgage transaction', 'Alternative mortgage transaction'),
-    ('Creditor', 'Creditor'),
-    ('State', 'State'),
-    ('State law', 'State law'),
+     ('Creditor', 'Creditor shall have the same meaning as in 12 CFR 226.2'),
 ]
 
 PARAGRAPH_HIERARCHY_1004 = {
-    '1004.2': [
-        1,
-        1,
-        2, 2, 2,
-        1,
-        1,
-        2, 2, 2, 2,
-        1,
-        1,
-    ],
+     '1004.2': [
+         1,
+         1,
+         2, 2, 2,
+         1,
+         1,
+         2, 2, 2, 2,
+         1,
+         1,
+     ],
 }
